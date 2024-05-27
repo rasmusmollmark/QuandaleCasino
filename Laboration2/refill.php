0ad90850a8e9134f92dc6d19b4e489c24f771739
Rasmus Möllmark <rasmusmollmark@gmail.com>
2024-05-27 17:16:52 +0200

    looool

--- Laboration2/refill.php
+++ Laboration2/refill.php
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Refill Currency</title>
-    <link rel="stylesheet" href="refil.css">
+    <link rel="stylesheet" href="refill.css">
     <script>
         function navigateTo(url) {
             window.location.href = url;
